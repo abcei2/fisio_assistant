fedb6397180c22ff85c720ad2aa17454bb10144f
abcei2 <santiago.salgado.duque@gmail.com>
2021-07-23 12:27:43 -0500

    .

--- assistant/urls.py
+++ assistant/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from view import bot
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("bot/", bot(), name="bot"),
+    path("bot/", bot, name="bot"),
 ]
