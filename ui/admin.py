2cb3fd99ba113b30a7481ede06ed674db24416b8
abcei2 <santiago.salgado.duque@gmail.com>
2021-08-02 19:13:12 +0000

    showing to specialists user's who are patients.

--- ui/admin.py
+++ ui/admin.py
@@ -7,5 +7,11 @@ from ui.models import User
 class UserAdmin(admin.ModelAdmin):
     
     list_display = ['whatsapp_number','notified', 'authorized','first_join']
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return query_session_timer
+        return qs.filter(groups__name = "patient")
 
 admin.site.register(User, UserAdmin)
