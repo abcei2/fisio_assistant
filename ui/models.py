b0524b4351fdf522d68ddf40aeb5fc12a5deaf14
abcei2 <santiago.salgado.duque@gmail.com>
2021-08-02 19:14:03 +0000

    Adding authourization_time to controll the 24h windows for free way message

--- ui/models.py
+++ ui/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from core.model_utils import BaseModel  
+from datetime import timedelta
 
 class Entity(BaseModel):
     legal_id = models.CharField(max_length=64, unique=True)
@@ -13,6 +14,16 @@ class User(AbstractUser, BaseModel):
     notified = models.BooleanField(default=False, verbose_name="El usuario ha sido notificado")
     authorized = models.BooleanField(default=False, verbose_name="Se recibe confirmación para mensajería libre/acepta términos")
     first_join = models.BooleanField(default=True, verbose_name="El usuario ha autorizado recibir mensajes")
+
+    authorization_time = models.DateTimeField(auto_now_add=True, verbose_name="Tiempo en que el usuario autoriza recibir mensajes" )    
+
+    def free_way_messages(self):
+        print(self.authorization_time, self.authorization_time > timedelta(days = 1), self.authorized ,self.notified)
+        if (self.authorized or self.notified ) and self.authorization_time > timedelta(days = 1):
+            self.authorized = False
+            self.notified = False
+
+    
 
 class UserEntity(BaseModel):
     entity = models.ForeignKey(Entity, on_delete=models.CASCADE, verbose_name="Entidad")
